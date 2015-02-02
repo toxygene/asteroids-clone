f1c7147b45ec74619e652d7b0360dd8b89c49813
Justin Hendrickson <justin.hendrickson@gmail.com>
2015-02-01 20:22:59 -0600

    Refactored momentum into a vector class.

--- game.js
+++ game.js
@@ -39,92 +39,47 @@
 		this.game = game;
 		this.gameSize = gameSize;
 		this.center = { x: gameSize.x / 2, y: gameSize.y / 2 };
-		this.angle = 0;
+		this.angle = -Math.PI/2;
+		this.momentum = new CanvasVector(0, 0);
 		this.size = { x: 10, y: 10 };
 		this.keyboard = new Keyboard();
-		this.bullets = [];
 		this.speed = 0;
 		this.canShoot = true;
 
-        setInterval(function() { this.speed = Math.max(0, this.speed - .1); }.bind(this), 100);
+		setInterval(function() {
+			this.momentum = this.momentum.subtractMagnatude(.1);
+		}.bind(this), 100);
 	};
 	
-	Player.prototype.draw = function(screen, gameSize) {
+	Player.prototype.draw = function(screen, gameSize) {	
 		screen.save();
 		screen.translate(this.center.x, this.center.y);
 		screen.rotate(this.angle);
 		screen.strokeStyle = "#FFFFFF";
 		screen.beginPath();
-		screen.moveTo(0, -10);
-		screen.lineTo(-6, 10);
-		screen.lineTo(6, 10);
+		screen.moveTo(10, 0);
+		screen.lineTo(-10, 6);
+		screen.lineTo(-10, -6);
 		screen.closePath();
 		screen.stroke();
 		screen.restore();
-		
-		this.bullets.forEach(function(bullet) {
-			bullet.draw(screen, gameSize);
-		});
-	};
-	
-	Player.prototype.fireBullet = function() {
-	    if (this.canShoot) {
-	        this.canShoot = false;
-            setTimeout(function() { this.canShoot = true; }.bind(this), 250);
-	        this.bullets.push(new Bullet({ x: this.getGunPosition().x, y: this.getGunPosition().y }, this.angle, this.speed));
-        }
-	};
-	
-	function throttle(callback, timeout) {
-	    return function() {
-	        if (typeof this.throttled === 'undefined') {
-	            this.throttled = false;
-            }
-            
-            if (!this.throttled) {
-                this.throttled = true;
-                setTimeout(function() { this.throttled = false; }.bind(this), timeout);
-    	        callback();
-	        }
-	    };
-	}
-	
-	Player.prototype.getGunPosition = function() {
-	    return {
-	        x: this.center.x + (10 * Math.sin(this.angle)),
-	        y: this.center.y - (10 * Math.cos(this.angle))
-        };
 	};
 
 	Player.prototype.update = function() {
 		if (this.keyboard.isDown(this.keyboard.KEYS.LEFT)) {
-			this.angle -= (Math.PI/3)/12;
+			this.angle -= Math.PI/36;
 		}
 		
 		if (this.keyboard.isDown(this.keyboard.KEYS.RIGHT)) {
-			this.angle += (Math.PI/3)/12;
+			this.angle += Math.PI/36;
 		}
-		
-		this.angle = ((this.angle % (2*Math.PI)) + (2*Math.PI)) % (2*Math.PI);
 		
 		if (this.keyboard.isDown(this.keyboard.KEYS.UP)) {
-			this.speed = 1.75;
+			this.momentum = this.momentum.addMagnatudeAndAngle(.5, this.angle).limitMagnatude(2.5);
 		}
 
-		this.center.x += this.speed * Math.sin(0 + this.angle);
-		this.center.y -= this.speed * Math.cos(0 + this.angle);
-		
-		if (this.keyboard.isDown(this.keyboard.KEYS.SPACE)) {
-			this.fireBullet();
-		}
-		
-		this.bullets.forEach(function(bullet) {
-			bullet.update();
-		});
-		
-		this.bullets = this.bullets.filter(function(bullet) {
-			return !bullet.isValid();
-		});
+		this.center.x += this.momentum.x;
+		this.center.y += this.momentum.y;
 	};
 	
 	var Keyboard = function() {
@@ -145,36 +100,46 @@
 
 	Keyboard.prototype.KEYS = { LEFT: 37, UP: 38, RIGHT: 39, DOWN: 40, SPACE: 32 };
 	
-	var Bullet = function(center, angle, speed) {
-		this.timer = 0;
-		this.center = center;
-		this.angle = angle;
-		this.speed = speed;
+	function CanvasVector(x, y) {
+		this.x = x;
+		this.y = y;
 	};
 	
-	Bullet.prototype.draw = function(screen, gameSize) {
-		screen.save();
-		screen.translate(this.center.x, this.center.y);
-		screen.rotate(this.angle);
-		screen.strokeStyle = "#FFFFFF";
-		screen.beginPath();
-		screen.moveTo(0, 0);
-		screen.lineTo(0, 4);
-		screen.closePath();
-		screen.stroke();
-		screen.restore();
+	CanvasVector.prototype.magnatude = function() {
+		return Math.sqrt((this.x*this.x)+(this.y*this.y));
 	};
 	
-	Bullet.prototype.isValid = function() {
-		return this.timer == 100;
-	}
-	
-	Bullet.prototype.update = function() {
-		++this.timer;
-		this.center.y -= (this.speed + 2.25) * Math.cos(0 + this.angle);
-		this.center.x += (this.speed + 2.25) * Math.sin(0 + this.angle);
+	CanvasVector.prototype.angle = function() {
+		return Math.atan2(this.y, this.x);
 	};
 	
+	CanvasVector.prototype.add = function(that) {
+		return new CanvasVector(this.x+that.x, this.y+that.y);
+	};
+	
+	CanvasVector.prototype.limitMagnatude = function(magnatude) {
+		if (this.magnatude() <= magnatude) {
+			return this;
+		}
+		
+		var x = magnatude*Math.cos(this.angle());
+		var y = magnatude*Math.sin(this.angle())
+		
+		return new CanvasVector(x, y);
+	};
+	
+	CanvasVector.prototype.subtractMagnatude = function(amount) {
+		if (this.magnatude() - amount < 0) {
+			return new CanvasVector(0, 0);
+		}
+		
+		return this.limitMagnatude(this.magnatude() - amount);
+	};
+	
+	CanvasVector.prototype.addMagnatudeAndAngle = function(magnatude, angle) {
+		return this.add(new CanvasVector(magnatude*Math.cos(angle),magnatude*Math.sin(angle)));
+	};
+
 	window.onload = function() {
 		new Asteroids('screen');
 	};
