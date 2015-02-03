90f9b8b96a1c01bdb241578d182fdb1dd1e9ec4e
Justin Hendrickson <justin.hendrickson@gmail.com>
2015-02-02 23:14:24 -0600

    Vector, bullet, and screen wrapping updates
    Refactored vector to use magnatude/angle. Added bullets. Added screen
    wrapping.

--- game.js
+++ game.js
@@ -43,17 +43,17 @@
 		this.momentum = new CanvasVector(0, 0);
 		this.size = { x: 10, y: 10 };
 		this.keyboard = new Keyboard();
-		this.speed = 0;
-		this.canShoot = true;
-
+		this.bullets = [];
+		this.attemptToFireBullet = this.attemptToFireBullet.bind(this).throttle(250);
+		
 		setInterval(function() {
-			this.momentum = this.momentum.subtractMagnatude(.1);
-		}.bind(this), 100);
+			this.momentum = this.momentum.addMagnatude(-.1).magnatudeRange(0, 3.75);
+		}.bind(this), 75);
 	};
 	
 	Player.prototype.draw = function(screen, gameSize) {	
 		screen.save();
-		screen.translate(this.center.x, this.center.y);
+		screen.translate(this.center.x.modulo(gameSize.x), this.center.y.modulo(gameSize.y));
 		screen.rotate(this.angle);
 		screen.strokeStyle = "#FFFFFF";
 		screen.beginPath();
@@ -63,6 +63,10 @@
 		screen.closePath();
 		screen.stroke();
 		screen.restore();
+		
+		this.bullets.forEach(function(bullet) {
+			bullet.draw(screen, gameSize);
+		});
 	};
 
 	Player.prototype.update = function() {
@@ -75,11 +79,47 @@
 		}
 		
 		if (this.keyboard.isDown(this.keyboard.KEYS.UP)) {
-			this.momentum = this.momentum.addMagnatudeAndAngle(.5, this.angle).limitMagnatude(2.5);
+			this.momentum = this.momentum.add(new CanvasVector(.5, this.angle)).magnatudeRange(0, 2);
+		}
+		
+		if (this.keyboard.isDown(this.keyboard.KEYS.SPACE)) {
+			this.attemptToFireBullet();
 		}
 
-		this.center.x += this.momentum.x;
-		this.center.y += this.momentum.y;
+		this.center.x += this.momentum.getX();
+		this.center.y += this.momentum.getY();
+		
+		this.bullets = this.bullets.filter(function(bullet) {
+			return bullet.isValid();
+		});
+
+		this.bullets.forEach(function(bullet) {
+			bullet.update();
+		});
+	};
+	
+	Player.prototype.attemptToFireBullet = function() {
+		this.bullets.push(new Bullet(this.center, new CanvasVector(this.momentum.getMagnatude(), this.angle)));
+		
+		return true;
+	};
+	
+	Function.prototype.throttle = function(timeout) {
+		return function() {
+			if (typeof this.throttled === 'undefined') {
+				this.throttled = false;
+			}
+			
+			if (!this.throttled) {
+				this.throttled = true;
+				setTimeout(function() { this.throttled = false; }.bind(this), 350);
+				this.call();
+			}
+		}.bind(this);
+	};
+	
+	Number.prototype.modulo = function(n) {
+		return ((this%n)+n)%n;
 	};
 	
 	var Keyboard = function() {
@@ -100,44 +140,74 @@
 
 	Keyboard.prototype.KEYS = { LEFT: 37, UP: 38, RIGHT: 39, DOWN: 40, SPACE: 32 };
 	
-	function CanvasVector(x, y) {
-		this.x = x;
-		this.y = y;
+	function Bullet(center, momentum) {
+		this.center = { x: center.x, y: center.y };
+		this.momentum = momentum.addMagnatude(5);
+		this.valid = true;
+		
+		setTimeout(function() { this.valid = false; }.bind(this), 1750);
 	};
 	
-	CanvasVector.prototype.magnatude = function() {
-		return Math.sqrt((this.x*this.x)+(this.y*this.y));
+	Bullet.prototype.draw = function(screen, gameSize) {
+		screen.save();
+		screen.translate(this.center.x.modulo(gameSize.x), this.center.y.modulo(gameSize.y));
+		screen.fillStyle = '#FFFFFF';
+		screen.fillRect(-2, -2, 2, 2);
+		screen.restore();
 	};
 	
-	CanvasVector.prototype.angle = function() {
-		return Math.atan2(this.y, this.x);
+	Bullet.prototype.isValid = function() {
+		return this.valid;
+	};
+	
+	Bullet.prototype.update = function() {
+		this.center.x += this.momentum.getX();
+		this.center.y += this.momentum.getY();
+	};
+	
+	function CanvasVector(magnatude, angle) {
+		this.magnatude = magnatude;
+		this.angle = angle;
+	};
+	
+	CanvasVector.prototype.getX = function() {
+		return this.getMagnatude() * Math.cos(this.getAngle());
+	};
+	
+	CanvasVector.prototype.getY = function() {
+		return this.getMagnatude() * Math.sin(this.getAngle());
+	};
+	
+	CanvasVector.prototype.getMagnatude = function() {
+		return this.magnatude;
+	};
+	
+	CanvasVector.prototype.getAngle = function() {
+		return this.angle;
 	};
 	
 	CanvasVector.prototype.add = function(that) {
-		return new CanvasVector(this.x+that.x, this.y+that.y);
+		var x = this.getX() + that.getX();
+		var y = this.getY() + that.getY();
+		
+		var magnatude = Math.sqrt((x*x)+(y*y));
+		var angle = Math.atan2(y, x);
+		
+		return new CanvasVector(magnatude, angle);
 	};
 	
-	CanvasVector.prototype.limitMagnatude = function(magnatude) {
-		if (this.magnatude() <= magnatude) {
-			return this;
+	CanvasVector.prototype.addMagnatude = function(magnatude) {
+		return new CanvasVector(this.getMagnatude() + magnatude, this.getAngle());
+	};
+	
+	CanvasVector.prototype.magnatudeRange = function(min, max) {
+		if (this.getMagnatude() < min) {
+			return new CanvasVector(min, this.getAngle());
+		} else if (this.getMagnatude() > max) {
+			return new CanvasVector(max, this.getAngle());
 		}
 		
-		var x = magnatude*Math.cos(this.angle());
-		var y = magnatude*Math.sin(this.angle())
-		
-		return new CanvasVector(x, y);
-	};
-	
-	CanvasVector.prototype.subtractMagnatude = function(amount) {
-		if (this.magnatude() - amount < 0) {
-			return new CanvasVector(0, 0);
-		}
-		
-		return this.limitMagnatude(this.magnatude() - amount);
-	};
-	
-	CanvasVector.prototype.addMagnatudeAndAngle = function(magnatude, angle) {
-		return this.add(new CanvasVector(magnatude*Math.cos(angle),magnatude*Math.sin(angle)));
+		return this;
 	};
 
 	window.onload = function() {
