7a467cc419f6d55297c4d43e1b7110151c925937
Justin Hendrickson <justin.hendrickson@nerdery.com>
2016-06-17 10:31:11 -0500

    Added ghost drawings of asteroids and ship

--- scripts/Asteroid.js
+++ scripts/Asteroid.js
@@ -5,34 +5,34 @@ define(function(require) {
         var angleSteps = [];
         var numberAngles = 2 * Math.PI / vertices;
         var sum = 0;
-        
+
         for (var i = 0; i < vertices; ++i) {
             var tmp = numberAngles;
             angleSteps.push(tmp);
             sum += tmp;
         }
-        
+
         var k = sum / (2 * Math.PI);
         for (var i = 0; i < vertices; ++i) {
             angleSteps[i] = angleSteps[i] / k;
         }
-        
+
         var points = [];
         var angle = Math.random(0, 2 * Math.PI);
-        
+
         for (var i = 0; i < vertices; ++i) {
             var r_i = Math.max(0, Math.min(Math.randomGaussian(40, 20), 80));
             var x = r_i * Math.cos(angle);
             var y = r_i * Math.sin(angle);
-            
+
             points.push({ x: parseInt(x), y: parseInt(y) });
-            
+
             angle += angleSteps[i];
         }
-        
+
         return points;
     };
-    
+
     function Asteroid(center, momentum) {
         this.center = center;
         this.momentum = momentum;
@@ -44,21 +44,91 @@ define(function(require) {
         this.center.x += this.momentum.getX();
         this.center.y += this.momentum.getY();
     };
-    
+
     Asteroid.prototype.draw = function(screen, gameSize) {
+        this.drawAsteroid(screen, gameSize)
+            .drawGhosts(screen, gameSize);
+    };
+
+    Asteroid.prototype.drawAsteroid = function(screen, gameSize) {
         screen.save();
-        screen.translate(modulo(this.center.x, gameSize.x), modulo(this.center.y, gameSize.y));
+
         screen.strokeStyle = "#FFFFFF";
-        
+        screen.translate(modulo(this.center.x, gameSize.x), modulo(this.center.y, gameSize.y));
         screen.moveTo(this.points[0].x, this.points[0].y);
+
+        screen.beginPath();
         this.points.slice(1).forEach(function(point) {
             screen.lineTo(point.x, point.y);
         });
-        
+        screen.closePath();
+
+        screen.stroke();
+
+        screen.restore();
+
+        return this;
+    };
+
+    Asteroid.prototype.drawGhosts = function(screen, gameSize) {
+        screen.save();
+
+        // left
+        screen.strokeStyle = "#FFFFFF";
+        screen.translate(modulo(this.center.x, gameSize.x) - gameSize.x, modulo(this.center.y, gameSize.y));
+        screen.moveTo(this.points[0].x, this.points[0].y);
+        screen.beginPath();
+        this.points.slice(1).forEach(function(point) {
+            screen.lineTo(point.x, point.y);
+        });
+        screen.closePath();
+        screen.stroke();
+
+        screen.restore();
+        screen.save();
+
+        // top
+        screen.strokeStyle = "#FFFFFF";
+        screen.translate(modulo(this.center.x, gameSize.x), modulo(this.center.y, gameSize.y) - gameSize.y);
+        screen.moveTo(this.points[0].x, this.points[0].y);
+        screen.beginPath();
+        this.points.slice(1).forEach(function(point) {
+            screen.lineTo(point.x, point.y);
+        });
+        screen.closePath();
+        screen.stroke();
+
+        screen.restore();
+        screen.save();
+
+        // right
+        screen.strokeStyle = "#FFFFFF";
+        screen.translate(modulo(this.center.x, gameSize.x) + gameSize.x, modulo(this.center.y, gameSize.y));
+        screen.moveTo(this.points[0].x, this.points[0].y);
+        screen.beginPath();
+        this.points.slice(1).forEach(function(point) {
+            screen.lineTo(point.x, point.y);
+        });
+        screen.closePath();
+        screen.stroke();
+
+        screen.restore();
+        screen.save();
+
+        // bottom
+        screen.strokeStyle = "#FFFFFF";
+        screen.translate(modulo(this.center.x, gameSize.x), modulo(this.center.y, gameSize.y) + gameSize.y);
+        screen.moveTo(this.points[0].x, this.points[0].y);
+        screen.beginPath();
+        this.points.slice(1).forEach(function(point) {
+            screen.lineTo(point.x, point.y);
+        });
         screen.closePath();
         screen.stroke();
         screen.restore();
+
+        return this;
     };
-    
+
     return Asteroid;
 });
--- scripts/Player.js
+++ scripts/Player.js
@@ -15,15 +15,42 @@ define(function(require) {
         this.keyboard = new Keyboard();
         this.bullets = [];
         this.attemptToFireBullet = throttle(this.attemptToFireBullet.bind(this), 250);
-        
+
         setInterval(function() {
             this.momentum = this.momentum.addMagnatude(-.1).magnatudeRange(0, 3.5);
         }.bind(this), 50);
     };
-    
-    Player.prototype.draw = function(screen, gameSize) {    
+
+    Player.prototype.draw = function(screen, gameSize) {
+        this.drawShip(screen, gameSize)
+            .drawGhostShips(screen, gameSize)
+            .drawBullets(screen, gameSize);
+    }
+
+    Player.prototype.drawShip = function(screen, gameSize) {
         screen.save();
+
         screen.translate(modulo(this.center.x, gameSize.x), modulo(this.center.y, gameSize.y));
+        screen.rotate(this.angle);
+        screen.strokeStyle = "#FFFFFF";
+
+        screen.beginPath();
+        screen.moveTo(10, 0);
+        screen.lineTo(-10, 6);
+        screen.lineTo(-10, -6);
+        screen.closePath();
+
+        screen.stroke();
+
+        screen.restore();
+
+        return this;
+    };
+
+    Player.prototype.drawGhostShips = function(screen, gameSize) {
+        // left
+        screen.save();
+        screen.translate(modulo(this.center.x, gameSize.x) - gameSize.x, modulo(this.center.y, gameSize.y));
         screen.rotate(this.angle);
         screen.strokeStyle = "#FFFFFF";
         screen.beginPath();
@@ -33,7 +60,50 @@ define(function(require) {
         screen.closePath();
         screen.stroke();
         screen.restore();
-        
+
+        // top
+        screen.save();
+        screen.translate(modulo(this.center.x, gameSize.x), modulo(this.center.y, gameSize.y) - gameSize.y);
+        screen.rotate(this.angle);
+        screen.strokeStyle = "#FFFFFF";
+        screen.beginPath();
+        screen.moveTo(10, 0);
+        screen.lineTo(-10, 6);
+        screen.lineTo(-10, -6);
+        screen.closePath();
+        screen.stroke();
+        screen.restore();
+
+        // right
+        screen.save();
+        screen.translate(modulo(this.center.x, gameSize.x) + gameSize.x, modulo(this.center.y, gameSize.y));
+        screen.rotate(this.angle);
+        screen.strokeStyle = "#FFFFFF";
+        screen.beginPath();
+        screen.moveTo(10, 0);
+        screen.lineTo(-10, 6);
+        screen.lineTo(-10, -6);
+        screen.closePath();
+        screen.stroke();
+        screen.restore();
+
+        // bottom
+        screen.save();
+        screen.translate(modulo(this.center.x, gameSize.x), modulo(this.center.y, gameSize.y) + gameSize.y);
+        screen.rotate(this.angle);
+        screen.strokeStyle = "#FFFFFF";
+        screen.beginPath();
+        screen.moveTo(10, 0);
+        screen.lineTo(-10, 6);
+        screen.lineTo(-10, -6);
+        screen.closePath();
+        screen.stroke();
+        screen.restore();
+
+        return this;
+    };
+
+    Player.prototype.drawBullets = function(screen, gameSize) {
         this.bullets.forEach(function(bullet) {
             bullet.draw(screen, gameSize);
         });
@@ -43,22 +113,22 @@ define(function(require) {
         if (this.keyboard.isDown(this.keyboard.KEYS.LEFT)) {
             this.angle -= Math.PI/36;
         }
-        
+
         if (this.keyboard.isDown(this.keyboard.KEYS.RIGHT)) {
             this.angle += Math.PI/36;
         }
-        
+
         if (this.keyboard.isDown(this.keyboard.KEYS.UP)) {
             this.momentum = this.momentum.add(new CanvasVector(.15, this.angle)).magnatudeRange(0, 3.5);
         }
-        
+
         if (this.keyboard.isDown(this.keyboard.KEYS.SPACE)) {
             this.attemptToFireBullet();
         }
 
         this.center.x += this.momentum.getX();
         this.center.y += this.momentum.getY();
-        
+
         this.bullets = this.bullets.filter(function(bullet) {
             return bullet.isValid();
         });
@@ -67,10 +137,10 @@ define(function(require) {
             bullet.update();
         });
     };
-    
+
     Player.prototype.attemptToFireBullet = function() {
         this.bullets.push(new Bullet(this.center, new CanvasVector(this.momentum.getMagnatude(), this.angle)));
-        
+
         return true;
     };
 
